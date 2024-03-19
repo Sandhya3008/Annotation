fc3b560fd5abab07497581299d8293bf1307b78a
susandhy <susandhy@LIN-GP89KN3.corp.capgemini.com>
2024-03-19 12:04:39 +0530

    First commit

---
+++ src/main/java/com/cg/demo/TextEditor.java
@@ -0,0 +1,22 @@
+package com.cg.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor {
+   private SpellChecker sp;
+   
+   @Autowired
+   public TextEditor(SpellChecker sp) {
+	  this.sp=sp; 
+   }
+   public SpellChecker getSp() {
+	   return sp;
+   }
+   public void setSp(SpellChecker sp) {
+	   this.sp=sp;
+   }
+   void showTextEditor() {
+	   System.out.println("this is method of TX");
+	   sp.showSpelling();
+   }
+}
---
+++ src/main/java/com/cg/emp/EmployeeConfig.java
@@ -0,0 +1,10 @@
+package com.cg.emp;
+
+import org.springframework.context.annotation.Bean;
+
+public class EmployeeConfig {
+  @Bean
+  public Employee getEmp() {
+	  return new Employee();
+  }
+}
